a59f7fa4e6f3e9097041eabf904900465dc633cd
cuongle30 <cuongle30@gmail.com>
2019-05-17 00:51:35 -0700

    update add new inv

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,6 @@
 var mysql = require('mysql')
 var inquirer = require('inquirer')
 
-
 var connection = mysql.createConnection({
     host: 'localhost',
 
@@ -21,7 +20,7 @@ connection.connect(function (err) {
     console.log('connected as id ' + connection.threadId + '\n')
     showOptions();
 })
-
+//Display Main Menu for avaialble options
 function showOptions() {
     inquirer.prompt({
         name: 'options',
@@ -38,11 +37,9 @@ function showOptions() {
         } else if (value.options === 'Add New Product') {
             addNewInv();
         }
-
     })
 }
-
-
+//Open table of available products
 function viewProducts() {
     // query the database for all items being auctioned
     connection.query('SELECT sku,product_name,price,stock_quantity FROM products', function (err, results) {
@@ -51,7 +48,7 @@ function viewProducts() {
         connection.end()
     })
 }
-
+//Function to view inventory under preset amount, 20.
 function viewLowInv() {
     connection.query('SELECT sku,product_name,price,stock_quantity FROM products WHERE stock_quantity <=20', function (err, results) {
         if (err) throw err
@@ -59,7 +56,7 @@ function viewLowInv() {
         connection.end()
     })
 }
-
+//Adding to Add to Inventory
 function addInv() {
     connection.query('SELECT sku,product_name,price,stock_quantity FROM products', function (err, results) {
         inquirer.prompt([
@@ -79,6 +76,7 @@ function addInv() {
                 //Update stock_quantity for added sku 
                 connection.query('SELECT sku,product_name,price,stock_quantity FROM products WHERE sku=?', function (err, results) {
                     if (err) throw err
+                    //Add user entered stock quantity to current stock quantity
                     var addedInv = value.parseInt(results[0].stock_quantity + parseInt(value.quantity))
                     console.log(`${addedInv} added to ${results[0].product_name}`)
                     connection.end();
@@ -90,56 +88,56 @@ function addInv() {
 }
 //Adding new inventory
 function addNewInv() {
-	inquirer.prompt([
-		{
-			type: 'input',
-			name: 'product_name',
-			message: 'Enter product name: ',
-		},
-		{
-			type: 'input',
-			name: 'department_name',
-			message: 'Enter department for product: ',
-		},
-		{
-			type: 'input',
-			name: 'price',
-			message: 'Enter price for unit: ',
-		},
-		{
-			type: 'input',
-			name: 'stock_quantity',
-			message: 'Enter quantity: '
-		}
-	]).then(function(input) {
-		console.log('Adding New Item: \n    product_name = ' + input.product_name + '\n' +  
-									   '    department_name = ' + input.department_name + '\n' +  
-									   '    price = ' + input.price + '\n' +  
-									   '    stock_quantity = ' + input.stock_quantity);
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'product_name',
+            message: 'Enter product name: ',
+        },
+        {
+            type: 'input',
+            name: 'department_name',
+            message: 'Enter department for product: ',
+        },
+        {
+            type: 'input',
+            name: 'price',
+            message: 'Enter price for unit: ',
+        },
+        {
+            type: 'input',
+            name: 'stock_quantity',
+            message: 'Enter quantity: '
+        }
+        //Display user input and ask for confirmation
+    ]).then(function (input) {
+        console.log('')
+        console.log(`Product Name: ${input.product_name}`)
+        console.log(`Department Name:  ${input.department_name}`)
+        console.log(`Price: ${input.price}`)
+        console.log(`Quantity ${input.stock_quantity}`)
+        console.log('')
         inquirer.prompt([
-            {type: 'confirm',
-             name: 'confirm',
-             message: 'Is this correct?'
-        }
-        ])
-        if(input === true){
-            console.log('added')
-            connection.end()
-        }if(input === false){
-            showOptions();
-        connection.end()
-		// Add new product to the db
-		// connection.query('INSERT INTO products SET ?', input, function (error, results, fields) {
-		// 	if (error) throw error;
-
-		// 	console.log('New product has been added to the inventory under Item ID ' + results.insertId + '.');
-		// 	console.log("\n---------------------------------------------------------------------\n");
-
-		// 	// End the database connection
-        //     connection.end();
-        
-        // });
-        }
-  })
+            {
+                type: 'confirm',
+                name: 'confirm',
+                message: 'Is this correct?',
+            }
+        ]).then(function (user) {
+            if (user.confirm) {
+                //add new product, shows added confirmation, and open product table
+                connection.query('INSERT INTO products SET ?', input, function (err, results) {
+                    if (err) throw err;
+                    console.log('')
+                    console.log(`${input.product_name} has been added.`);
+                    console.log('')
+                    viewProducts();
+                })
+                //Will display main menu if user select no on confirmation
+            } else {
+                showOptions();
+            }
+        })
+    })
 }
 
